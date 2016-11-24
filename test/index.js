0b91c29da021366d6f9d617f176e0785b53dd0da
Tom Coleman <tom@thesnail.org>
2016-11-24 17:05:45 +1100

    Started on tests

--- test/index.js
+++ test/index.js
@@ -9,6 +9,9 @@ function sendQuery({ query }) {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({ query }),
+  }).then((response) => {
+    assert.equal(response.status, 200, response.statusText);
+    return response.json();
   });
 }
 
@@ -21,12 +24,39 @@ describe('environment', () => {
     }`;
 
     return sendQuery({ query, operationName: 'foo' })
-      .then((response) => {
-        assert.equal(response.status, 200);
-        return response.json();
-      })
       .then((result) => {
         assert.isDefined(result.data);
       });
+  });
+});
+
+describe('queries', () => {
+  function itQueries(name, query, expectedResult) {
+    it(name, () => {
+      return sendQuery({ query })
+        .then((result) => {
+          assert.isDefined(result.data);
+          assert.deepEqual(result.data, expectedResult);
+        });
+    });
+  }
+
+  describe('users', () => {
+    itQueries('basic data',
+      '{ user(id: 1) { id, username } }',
+      { user: { id: '1', username: 'tmeasday' } }
+    );
+
+    itQueries('relations',
+      '{ user(id: 1) { followers { username } following { username } } }',
+      { user: {
+        followers: [{ username: 'stubailo' }],
+        following: [{ username: 'stubailo' }, { username: 'lacker' }],
+      } }
+    );
+
+    // itQueries('user tweets')
+    // itQueries('basic pagination')
+    // itQueries('pagination w/ startDate')
   });
 });
