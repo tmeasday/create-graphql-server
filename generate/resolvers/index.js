bd9526a3a49ecf2cdeae5bcfc97c38b3a7b12714
Tom Coleman <tom@thesnail.org>
2016-11-28 17:22:37 +1100

    Working through resolver generation.

--- generate/resolvers/index.js
+++ generate/resolvers/index.js
@@ -1,11 +1,28 @@
+const SCALAR_TYPE_NAMES = ['Int', 'Float', 'String', 'ID'];
+
+function getBaseType(type) {
+  if (type.kind === 'ListType' || type.kind === 'NonNullType') {
+    return getBaseType(type.type);
+  }
+  return type;
+}
+
 export default function generateResolvers(inputSchema, outputSchema) {
   const resolvers = {};
 
-  outputSchema.definitions.forEach((definition) => {
+  inputSchema.definitions.forEach((definition) => {
     if (definition.kind === 'ObjectTypeDefinition') {
       resolvers[definition.name.value] = {};
       definition.fields.forEach((field) => {
+        if (SCALAR_TYPE_NAMES.includes(getBaseType(field.type).name.value)) {
+          return;
+        }
         resolvers[definition.name.value][field.name.value] = () => {
+          console.log(`${definition.name.value}${field.name.value}`);
+
+          if (field.type.kind === 'ListType') {
+            return [{}, {}];
+          }
           return {};
         };
       });
