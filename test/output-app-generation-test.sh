75cdd889f5d802b699278da89c52cd2204aa16f1
Tom Coleman <tom@thesnail.org>
2016-12-08 14:01:05 +1100

    Added a test to check if what we output matches the app

---
+++ test/output-app-generation-test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+pushd "$(dirname $0)/.." > /dev/null
+PACKAGE_DIR=`pwd`
+popd > /dev/null
+
+CGS="$PACKAGE_DIR/bin/create-graphql-server.js"
+INPUT_DIR="$PACKAGE_DIR/test/input"
+EXPECTED_OUTPUT_DIR="$PACKAGE_DIR/test/output-app"
+
+set -e
+
+TMPDIR=`mktemp -d 2>/dev/null || mktemp -d -t 'cgs-test'`
+function finish {
+  # rm -rf $TMPDIR
+  echo
+  echo
+  echo $TMPDIR
+  echo
+  echo "Test failed"
+}
+trap finish EXIT
+
+cd $TMPDIR
+$CGS init output-app
+cd output-app
+$CGS add-type "$INPUT_DIR/tweet.graphql"
+$CGS add-type "$INPUT_DIR/user.graphql"
+
+set +e
+
+diff -rb . "$EXPECTED_OUTPUT_DIR"
