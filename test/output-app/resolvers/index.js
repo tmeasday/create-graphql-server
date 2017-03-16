f77bbda7f4eea7d8324638fd1c98a64884efef14
Tom Coleman <tom@thesnail.org>
2017-03-16 14:35:26 +1100

    Fix issue with output app

--- test/output-app/resolvers/index.js
+++ test/output-app/resolvers/index.js
@@ -1,7 +1,7 @@
 import { ObjectId } from 'mongodb';
 import { GraphQLScalarType } from 'graphql';
 import { Kind } from 'graphql/language';
-import merge from 'lodash.merge';
+import { merge } from 'lodash';
 
 const resolvers = {};
 
